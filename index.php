1ac93f9e6bf4e3e16150535d32f0a1d960c2dd72
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-08-21 20:32:53 +0200

    add initial HTML structure and logo to index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CD's Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+</head>
+
+<body class="bg-dark text-white">
+    <header class="mb-5">
+        <nav
+            class="navbar navbar-expand-sm navbar-dark bg-dark border-bottom border-top border-left border-right border-success">
+            <div class="container">
+                <a class="navbar-brand" href="#"><img src="./img/ChatGPT Image 21 ago 2026, 20_18_53.png" alt="Logo App Musica" style="width: 90px; height: 60px;"></a>
+
+            </div>
+        </nav>
+
+    </header>
+
+    <main>
+        <div class="container">
+            <h1>I tuoi brani</h1>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                            <p class="card-text text-center">1999</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
